124221d80132365adb072d806f5ea11ca8b93d55
tusas <paulius.tusas98@gmail.com>
2022-03-03 15:53:53 +0200

    1 commit

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -8,18 +8,25 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvMain;
+    private boolean isDefaultValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.tvMain = (TextView)findViewById(R.id.tvMain);
-        this.tvMain.setText("Works as Main in Java");
+        this.tvMain = findViewById(R.id.tvMain);
+        this.isDefaultValue = true;
 
     }
 
     public void onBtnMainClick(View view) {
-        this.tvMain.setText("Slava Ukraine");
+        if(this.isDefaultValue){
+            this.tvMain.setText(R.string.changed_name);
+        }
+        else{
+            this.tvMain.setText(R.string.default_name);
+        }
+        this.isDefaultValue = !this.isDefaultValue;
     }
 }
